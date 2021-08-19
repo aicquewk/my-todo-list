88d819a661f4c47cbf9cb8e4a29fc0742a6e2791
Aicquewk <vuttithatkrongyot@gmail.com>
2021-08-19 21:00:22 +0700

    Updates

---
+++ precache-manifest.b466fe81500ed4994e393f9f92ebcf43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4495aaff6b5d918a84d27b0bb4223d6",
+    "url": "/my-todo-list/index.html"
+  },
+  {
+    "revision": "e8a5411914f366e62c76",
+    "url": "/my-todo-list/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "856aa4b9211ce8e1d10a",
+    "url": "/my-todo-list/static/js/2.bbef98fe.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/my-todo-list/static/js/2.bbef98fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8a5411914f366e62c76",
+    "url": "/my-todo-list/static/js/main.096e3dd0.chunk.js"
+  },
+  {
+    "revision": "01f3a19e2646451aaa1f",
+    "url": "/my-todo-list/static/js/runtime-main.c02c6521.js"
+  }
+]);
